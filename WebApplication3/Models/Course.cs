84254622072d5481e8e670694f56f5150a9404eb
Esraa Soliman <ee341110@gmail.com>
2025-08-07 22:26:03 +0300

    Add UI Validation

--- WebApplication3/Models/Course.cs
+++ WebApplication3/Models/Course.cs
@@ -6,6 +6,9 @@ namespace WebApplication3.Models
     public class Course
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Course name is required")]
+        [StringLength(100, ErrorMessage = "Course name must be less than 100 characters")]
         public string? CourseName { get; set; }
     }
 }
