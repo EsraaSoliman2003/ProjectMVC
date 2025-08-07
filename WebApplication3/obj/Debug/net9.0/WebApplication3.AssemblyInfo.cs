2a48f7ad0e23d1bf2bd946a79a38cb065c52bcee
Esraa Soliman <ee341110@gmail.com>
2025-08-07 23:13:28 +0300

    fix problem

--- WebApplication3/obj/Debug/net9.0/WebApplication3.AssemblyInfo.cs
+++ WebApplication3/obj/Debug/net9.0/WebApplication3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3f0f838dc7fb8fc8fe92b77c9f36b5fb6c45e33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+248ec2905bbe9e8260193a57b9c69eff05312521")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
